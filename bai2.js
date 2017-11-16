facb1010b250099abcdbc2c3c1112d5f2867db02
tuanminh537 <ngominhtuan.usd@gmail.com>
2017-11-16 21:35:22 +0700

    first commit

--- bai2.js
+++ bai2.js
@@ -3,10 +3,10 @@ window.onload = function(){
 	setTimeout ("switchImage()",3000);
 }
 var current =1;
-var num_image=4;
+var num_image=3;
 function switchImage(){
 	current++;
-	document.image['image'].src = 'image/car' + current + '.jpg';
+	document.getElementById('slider').src = 'image/car' + current + '.jpg';
 	if(current == num_image) { current=0; }
-	setTimeout("swithImage()",3000);
+	setTimeout("switchImage()",3000);
 }
